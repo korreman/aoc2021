b5a38bf8534e39c6514d3e85cb97841e8f8ba130
Troels Korreman <troels.korreman@gmail.com>
2021-12-23 01:58:49 +0100

    Day 22 init

---
+++ src/day22.rs
@@ -0,0 +1,6 @@
+pub fn run(input: &str) -> (u64, u64) {
+    // Input space is too large to store in an array.
+    // Seems like you could arrange cuboids into a tree structure.
+    // Like a binary tree or an octuple tree.
+    (0, 0)
+}
